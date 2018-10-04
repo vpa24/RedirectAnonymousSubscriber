ce3992e244643450876a20d0111e8458ae6cca5c
vpa24 <vuphuonganh020497@gmail.com>
2018-10-04 15:57:56 +0700

    Using dependency injection

--- README.md
+++
@@ -1,2 +0,0 @@
-# RedirectAnonymousSubscriber
-Drupal 8 | Redirect all anonymous users to login page. With a few needed exceptions like /user/password
--- src/EventSubscriber/RequestSubscriber.php
+++ src/EventSubscriber/RequestSubscriber.php
@@ -8,17 +8,30 @@ use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+
 class RequestSubscriber implements EventSubscriberInterface{
-   public function checkAuthStatus(GetResponseEvent $event) {
+
+  protected $routeMatch;
+  protected $accountProxy;
+ 
+  public function __construct(RouteMatchInterface $route_match, AccountProxyInterface $account_proxy){
+    $this->routeMatch = $route_match;
+    $this->accountProxy = $account_proxy;
+  }
+
+  public function checkAuthStatus(GetResponseEvent $event) {
     if (
-      \Drupal::currentUser()->isAnonymous() &&
-      \Drupal::routeMatch()->getRouteName() != 'user.login' ) {
+      $this->accountProxy->isAnonymous() &&
+      $this->routeMatch->getRouteName() != 'user.login' ) {
     
       $url = Url::fromRoute('user.login')->toString();
-      $response = new RedirectResponse($url, 301);
-      $response->send();
+      $redirect = new RedirectResponse($url, 301);
+      $event->setResponse($redirect);
     }
   }
+
   public static function getSubscribedEvents() {
     return [
             KernelEvents::REQUEST => ['checkAuthStatus']
