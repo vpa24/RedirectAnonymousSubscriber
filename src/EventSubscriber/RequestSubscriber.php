46779d96890982a96183d407d28de7b5bb5624bd
vpa24 <vuphuonganh020497@gmail.com>
2018-10-04 21:05:20 +0700

    home page is not redirect to login

--- src/EventSubscriber/RequestSubscriber.php
+++ src/EventSubscriber/RequestSubscriber.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Drupal\event\EventSubscriber;
 
 use Drupal\Core\Url;
@@ -7,34 +6,35 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Session\AccountProxyInterface;
 
-class RequestSubscriber implements EventSubscriberInterface{
-
+class RequestSubscriber implements EventSubscriberInterface
+{
   protected $routeMatch;
   protected $accountProxy;
- 
-  public function __construct(RouteMatchInterface $route_match, AccountProxyInterface $account_proxy){
+
+  public function __construct(RouteMatchInterface $route_match, AccountProxyInterface $account_proxy)
+  {
     $this->routeMatch = $route_match;
     $this->accountProxy = $account_proxy;
   }
+  public function checkAuthStatus(GetResponseEvent $event)
+  {
+    if ($this->routeMatch->getRouteName() == 'user.login'|| $this->routeMatch->getRouteName() == 'view.frontpage.page_1') {
+      return;
+    }
+    if ($this->accountProxy->isAnonymous()) {
 
-  public function checkAuthStatus(GetResponseEvent $event) {
-    if (
-      $this->accountProxy->isAnonymous() &&
-      $this->routeMatch->getRouteName() != 'user.login' ) {
-    
       $url = Url::fromRoute('user.login')->toString();
-      $redirect = new RedirectResponse($url, 301);
+      $redirect = new RedirectResponse($url);
       $event->setResponse($redirect);
     }
   }
-
-  public static function getSubscribedEvents() {
+  public static function getSubscribedEvents()
+  {
     return [
-            KernelEvents::REQUEST => ['checkAuthStatus']
-        ];
-    }
+      KernelEvents::REQUEST => ['checkAuthStatus']
+    ];
+  }
 }
